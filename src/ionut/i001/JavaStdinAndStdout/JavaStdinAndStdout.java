9ded2240d5da33277516d1aee034262042ed8795
Ion Craciunescu <icraciunescu@hotmail.com>
2019-10-16 19:36:27 +0300

    i002

--- src/ionut/i001/JavaStdinAndStdout/JavaStdinAndStdout.java
+++ src/ionut/i001/JavaStdinAndStdout/JavaStdinAndStdout.java
@@ -1,4 +1,4 @@
-package ionut;
+package ionut.i001.JavaStdinAndStdout;
 
 import java.util.Scanner;
 
@@ -10,16 +10,17 @@ public class JavaStdinAndStdout {
         int b = scan.nextInt();
         int c = scan.nextInt();
         scan.close();
-        // Complete this line
-        // Complete this line
 
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
-        // Complete this line
-        // Complete this line
+
     }
-    /* Most	HackerRank	challenges	require	you	to	read	input	from	stdin	(standard	input)	and	write	output	to stdout	(standard	output). One	popular	way	to	read	input	from	stdin	is	by	using	the	Scanner	class	and	specifying	the	I n p u t	S t r e a m	as S y s t e m.in .	For	example:
+    /*
+
+    https://www.hackerrank.com/challenges/java-stdin-and-stdout-1/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
+
+    Most	HackerRank	challenges	require	you	to	read	input	from	stdin	(standard	input)	and	write	output	to stdout	(standard	output). One	popular	way	to	read	input	from	stdin	is	by	using	the	Scanner	class	and	specifying	the	I n p u t	S t r e a m	as S y s t e m.in .	For	example:
     Scanner	scanner	=	new	Scanner(System.in); String	myString	=	scanner.next(); int	myInt	=	scanner.nextInt(); scanner.close(); System.out.println("myString	is:	"	+	myString); System.out.println("myInt	is:	"	+	myInt);
     The	code	above	creates	a	S c a n n e r	object	named		and	uses	it	to	read	a	S t rin g	and	an	in t .	It	then clo s e s	the	S c a n n e r	object	because	there	is	no	more	input	to	read,	and	prints	to	stdout	using S y s t e m. o u t. p rin tln ( S t rin g ) .	So,	if	our	input	is:
     Hi	5
@@ -30,4 +31,5 @@ public class JavaStdinAndStdout {
     Sample	Output
 42 100 125
      */
+
 }
