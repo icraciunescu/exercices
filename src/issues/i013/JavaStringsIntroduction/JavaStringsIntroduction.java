6b57e6635eeadcb9ba0f9413e0dce7af82b08a50
Ion Craciunescu <icraciunescu@hotmail.com>
2019-11-03 09:44:59 +0200

    i014

---
+++ src/issues/i013/JavaStringsIntroduction/JavaStringsIntroduction.java
@@ -0,0 +1,24 @@
+package issues.i013.JavaStringsIntroduction;
+
+import java.util.Scanner;
+
+public class JavaStringsIntroduction {
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        sc.close();
+        int sumOfLength = A.length() + B.length();
+        System.out.println(sumOfLength);
+        System.out.println((A.compareTo( B ) > 0 ? "Yes" : "No" ));
+        System.out.println(A.substring( 0, 1 ).toUpperCase() + A.substring( 1 ) + " " + B.substring( 0, 1 ).toUpperCase() + B.substring( 1 ));
+
+    }
+
+}
+
+/*
+https://www.hackerrank.com/challenges/java-strings-introduction/problem
+ */
