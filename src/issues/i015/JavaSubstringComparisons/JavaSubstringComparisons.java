26252eb7207ead3931062c7fdaf94e0dec6dced8
Ion Craciunescu <icraciunescu@hotmail.com>
2019-11-03 19:21:08 +0200

    i015

---
+++ src/issues/i015/JavaSubstringComparisons/JavaSubstringComparisons.java
@@ -0,0 +1,37 @@
+package issues.i015.JavaSubstringComparisons;
+
+import java.util.Scanner;
+
+public class JavaSubstringComparisons {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = s.substring( 0, k );
+        String largest = s.substring( 0, k );
+        String temp = "";
+        if (k < s.length()) {
+            for (int i = 0; i < s.length() - k + 1; i++) {
+                temp = s.substring( i, i + k );
+                if (smallest.compareTo( temp ) > 0) {
+                    smallest = temp;
+                }
+                if (largest.compareTo( temp ) < 0) {
+                    largest = temp;
+                }
+            }
+        }
+        return smallest + "\n" + largest;
+    }
+
+}
+
+/*
+https://www.hackerrank.com/challenges/java-string-compare/problem
+ */
