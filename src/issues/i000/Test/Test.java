ae380340289d7c283695870547cf382360366264
Ion Craciunescu <icraciunescu@hotmail.com>
2019-11-04 21:25:26 +0200

    i018

--- src/issues/i000/Test/Test.java
+++ src/issues/i000/Test/Test.java
@@ -1,5 +1,23 @@
 package issues.i000.Test;
 
+import java.util.Scanner;
+
 public class Test {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        for (int i = 0; i < a.length(); i++) {
+            String letter = String.valueOf( a.charAt( i ) );
+            if (!b.contains( letter )) {
+                //System.out.println(letter);
+            } else {
+                b = b.replace( letter, "" );
+                System.out.println(b);
+            }
+        }
+    }
 
 }
---
+++ src/issues/i018/JavaAnagramsII/JavaAnagramsII.java
@@ -0,0 +1,45 @@
+package issues.i018.JavaAnagramsII;
+
+import java.util.Scanner;
+
+public class JavaAnagramsII {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+
+    static boolean isAnagram(String a, String b) {
+        boolean anagram = true;
+
+        if (a == null || b == null || a.equals("") || b.equals("") || a.length() != b.length()) {
+            anagram = false;
+        } else  {
+            a = a.toLowerCase();
+            b = b.toLowerCase();
+            for (int i = 0; i < a.length(); i++) {
+                String letter = String.valueOf( a.charAt( i ) );
+                if (!b.contains( letter )) {
+                    anagram = false;
+                } else {
+                    b = b.replaceFirst( letter, "" );
+                }
+            }
+            if (b.equals( "" )) {
+                anagram = true;
+            }
+        }
+        return anagram;
+    }
+
+}
+
+/*
+https://www.hackerrank.com/challenges/java-anagrams/problem
+ */
+
