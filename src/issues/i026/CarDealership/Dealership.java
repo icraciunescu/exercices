341225bb1df9d075845cf0986c7f6d10604a958c
Ion Craciunescu <icraciunescu@hotmail.com>
2019-11-14 03:04:08 +0200

    i026

---
+++ src/issues/i026/CarDealership/Dealership.java
@@ -0,0 +1,23 @@
+package issues.i026.CarDealership;
+
+public class Dealership {
+
+    public static void main(String[] args) {
+
+        Customer customer = new Customer();
+        customer.setName( "tom" );
+        customer.setAddress( "123 address" );
+        customer.setCash( 24000 );
+
+        Vehicle vehicle = new Vehicle();
+        vehicle.setMake( "honda" );
+        vehicle.setModel( "accord" );
+        vehicle.setColor( "red" );
+        vehicle.setPrice( 14000 );
+        Employee employee = new Employee();
+
+        customer.purchaseCar( vehicle, employee, false );
+
+    }
+
+}
