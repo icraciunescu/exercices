02777bdf437bcd62c9073d58a40a7b06ff6143a1
Ion Craciunescu <icraciunescu@hotmail.com>
2019-11-27 21:02:38 +0200

    i031

---
+++ src/issues/i031/CarDealership/Dealership.java
@@ -0,0 +1,14 @@
+package issues.i031.CarDealership;
+
+public class Dealership {
+
+    public static void main(String[] args) {
+
+        Customer customer1 = new Customer("name_customer", "address_customer", 3000, 11000);
+        Car car1 = new Car( "make_car", "model_car", 15000 );
+        Employee employee1 = new Employee("nameEmp");
+        customer1.purchaseCar(customer1, car1, employee1);
+
+    }
+
+}
