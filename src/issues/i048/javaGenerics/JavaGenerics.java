44f00ef182d90457e3c30734d5c0930a34a07a09
Ion Craciunescu <icraciunescu@hotmail.com>
2019-12-29 09:16:16 +0200

    i048

---
+++ src/issues/i048/javaGenerics/JavaGenerics.java
@@ -0,0 +1,41 @@
+package issues.i048.javaGenerics;
+
+import java.lang.reflect.Method;
+
+public class JavaGenerics {
+
+    public static void main(String[] args) {
+
+        Printer myPrinter = new Printer();
+        Integer[] intArray = {1, 2, 3};
+        String[] stringArray = {"Hello", "World"};
+        myPrinter.printArray( intArray );
+        myPrinter.printArray( stringArray );
+        int count = 0;
+
+        for (Method method : Printer.class.getDeclaredMethods()) {
+            String name = method.getName();
+
+            if (name.equals( "printArray" ))
+                count++;
+        }
+
+        if (count > 1) System.out.println( "Method overloading is not allowed!" );
+
+    }
+
+}
+
+class Printer <E> {
+    //Write your code here
+    public <E> void printArray(E[] array) {
+        for (E element : array) {
+            System.out.println(element);
+        }
+    }
+
+}
+
+/*
+https://www.hackerrank.com/challenges/java-generics/problem
+ */
