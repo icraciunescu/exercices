2c5d57982cacacff9069b1ad709b9785eeb26ec7
Ion Craciunescu <icraciunescu@hotmail.com>
2019-12-29 19:10:50 +0200

    i056

---
+++ src/issues/i053/javaMethodOverriding/JavaMethodOverriding.java
@@ -0,0 +1,46 @@
+package issues.i053.javaMethodOverriding;
+
+public class JavaMethodOverriding {
+
+
+    public static void main(String[] args) {
+
+        Sports c1 = new Sports();
+        Soccer c2 = new Soccer();
+        System.out.println(c1.getName());
+        c1.getNumberOfTeamMembers();
+        System.out.println(c2.getName());
+        c2.getNumberOfTeamMembers();
+
+    }
+}
+
+class Sports{
+
+    String getName(){
+        return "Generic Sports";
+    }
+
+    void getNumberOfTeamMembers(){
+        System.out.println( "Each team has n players in " + getName() );
+    }
+}
+
+class Soccer extends Sports{
+    @Override
+    String getName(){
+        return "Soccer Class";
+    }
+
+    // Write your overridden getNumberOfTeamMembers method here
+    @Override
+    void  getNumberOfTeamMembers() {
+        System.out.println("Each team has " + 11 + " players in " + getName());
+    }
+
+}
+
+
+/*
+https://www.hackerrank.com/challenges/java-method-overriding/problem?h_r=next-challenge&h_v=zen
+ */
---
+++ src/issues/i056/javaExceptionHandlingTryCatch/JavaExceptionHandlingTryCatch.java
@@ -0,0 +1,36 @@
+package issues.i056.javaExceptionHandlingTryCatch;
+
+import java.util.Scanner;
+
+public class JavaExceptionHandlingTryCatch {
+
+    public static void main(String[] args) {
+
+        try {
+            int x;
+            int y;
+            Scanner scanner = new Scanner( System.in );
+            x = scanner.nextInt();
+            y = scanner.nextInt();
+            scanner.close();
+
+            divide( x, y );
+
+        } catch (ArithmeticException e) {
+            System.out.println("java.lang.ArithmeticException: / by zero");
+        } catch (Exception e) {
+            String str = e.getClass().getName();
+            System.out.println( str );
+        }
+
+    }
+
+    public static void divide(int a, int b) {
+        System.out.println( a / b );
+    }
+
+}
+
+/*
+https://www.hackerrank.com/challenges/java-exception-handling-try-catch/problem
+ */
