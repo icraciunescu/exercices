77814228d4332961081f0b4d87dcabcb5fa79fac
Ion Craciunescu <icraciunescu@hotmail.com>
2020-01-01 18:57:41 +0200

    i064

---
+++ src/issues/i063/javaStack/JavaStack.java
@@ -0,0 +1,24 @@
+package issues.i063.javaStack;
+
+import java.util.Scanner;
+
+public class JavaStack {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner( System.in );
+        while (sc.hasNext()) {
+            String input = sc.next();
+            //Complete the code
+            while (input.length() != (input = input.replaceAll( "\\(\\)|\\[\\]|\\{\\}", "" )).length()) ;
+            System.out.println( input.isEmpty() );
+        }
+
+    }
+
+}
+
+
+/*
+https://www.hackerrank.com/challenges/java-stack/problem
+ */
