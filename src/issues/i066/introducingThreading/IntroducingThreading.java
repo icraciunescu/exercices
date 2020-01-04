720c08f0b493c3ba9b7b3c52599a8d9fb4bf57d4
Ion Craciunescu <icraciunescu@hotmail.com>
2020-01-04 17:30:15 +0200

    i068

---
+++ src/issues/i066/introducingThreading/IntroducingThreading.java
@@ -0,0 +1,39 @@
+package issues.i066.introducingThreading;
+
+public class IntroducingThreading {
+
+    public static void main(String[] args) {
+
+        Task task = new Task("thread a");
+        task.start();
+
+        System.out.println("hello !!!");
+
+        Task task1 = new Task("thread b");
+        task1.start();
+    }
+
+}
+
+class Task extends Thread {
+
+    String name;
+
+    public Task(String name) {
+        this.name = name;
+    }
+
+    public void run() {
+        Thread.currentThread().setName( this.name );
+        for (int i = 1; i < 1000; i++) {
+            System.out.println(Thread.currentThread().getName(  ) + " - " + i);
+            try {
+                Thread.sleep( 10 );
+            } catch (InterruptedException e) {
+                System.out.println(e.getClass().getName());
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
